5156c1bff5d43dbb672b44cd7d3dff452904ea38
Eddy <61814371+eddyxwong@users.noreply.github.com>
2022-08-01 22:44:37 +1000

    CORS WORKS

--- src/components/UploadFile.js
+++ src/components/UploadFile.js
@@ -6,10 +6,7 @@ import InputInstructions from '../components/InputInstructions';
 import Fetch from 'fetch';
 
 export default function UploadFile(props) {
-    // let { acceptedFiles, fileRejections, getRootProps, getInputProps } =
-    //     useDropzone({
-    //         accept: ['.py'],
-    //     });
+    let [policy, setPolicy] = useState('');
     const { getRootProps, getInputProps } = useDropzone({
         accept: {
             'text/python': ['.py'],
@@ -17,68 +14,48 @@ export default function UploadFile(props) {
         onDrop: (e) => {
             handleOnDrop(e);
         },
-        // multiple: 'True',
     });
-    // console.log(acceptedFiles);
     let filekey = Math.floor(Math.random() * 1000000000);
-    const filename = filekey.toString();
-    let content = null;
 
     const handleOnDrop = (files) => {
         const zip = require('jszip')();
-        console.log(files);
-        console.log('fk me ');
         for (let i = 0; i < files.length; i++) {
             zip.file(files[i].name, files[i]);
         }
 
         let uri =
-            'https://2ivhlotb6k.execute-api.us-east-1.amazonaws.com/dev/static-iam-store-simplified/';
+            'https://fz41s9yjre.execute-api.ap-southeast-2.amazonaws.com/UAT/triggerscriptanalyzer';
         zip.generateAsync({ type: 'blob' }).then((blob) => {
+            let requestOptions = {
+                method: 'OPTIONS',
+                'Access-Control-Allow-Origin': '*',
+                // 'Access-Control-Allow-Methods':
+                // 'GET, PUT, PATCH, POST, DELETE, OPTIONS',
+                // 'Access-Control-Allow-Headers': 'Authorization, Content-Type',
+                redirect: 'follow',
+            };
+            fetch(uri, requestOptions)
+                .then((e) => {
+                    console.log(e);
+                    return e.json();
+                })
+                .then((e) => {
+                    console.log(e);
+                    // setPolicy(e);
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
+
             // 1) generate the zip file
             // console.log(blob);
             // fetch('google.com').then((e) => console.log(e));
             // fetch ()
         });
-
-        // fetch(
-        //     `https://2ivhlotb6k.execute-api.us-east-1.amazonaws.com/dev/static-iam-store-simplified/${filename}.zip`,
-        //     {
-        //         method: 'PUT',
-        //         headers: {},
-        //     }
-        // ).then((res) => {
-        //     if (res.ok) {
-        //         console.log('HTTP PUT SUCCESS');
-        //     } else {
-        //         console.log('HTTP PUT FAIL');
-        //     }
-        // });
     };
-
-    // return (
-    //     <div className={props.class}>
-    //         <h2>{props.title}</h2>
-    //         <Dropzone multiple={true} onDrop={handleOnDrop}>
-    //             {({ getRootProps, getInputProps }) => (
-    //                 <section className="Drop">
-    //                     <div {...getRootProps()}>
-    //                         <input
-    //                             {...getInputProps()}
-    //                             hidden
-    //                             // accept=".py"
-    //                             multiple
-    //                             type="file"
-    //                         />
-    //                         <InputInstructions />
-    //                     </div>
-    //                 </section>
-    //             )}
-    //         </Dropzone>
-    //     </div>
-    // );
     return (
         <div className={props.class}>
+            <h1>{policy}</h1>
             <h2>{props.title}</h2>
             <section className="Drop">
                 <section className="container">
